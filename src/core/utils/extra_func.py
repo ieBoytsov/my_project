a96029039bc8e9b81f43e298a91420cbd5e3066b
i.boytsov <i.boytsov@MAC867.local>
2020-03-22 00:55:56 +0300

    add distance_to_collision_task and refactor code

---
+++ src/core/utils/extra_func.py
@@ -0,0 +1,10 @@
+import errno
+import os
+
+
+def mkdir_if_missing(dir_path):
+    try:
+        os.makedirs(dir_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
